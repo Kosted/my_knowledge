7360efb6aa39596fc1a14406e0da735b8bc56115
kosted <konstantinus@gmail.com>
2020-03-10 18:23:24 +0300

    update tag and memory uppon adding RegularUser model

--- knowledge/models.py
+++ knowledge/models.py
@@ -9,8 +9,30 @@ from django.db import models
 from django.utils import timezone
 
 
+class RegularUser(User):
+    last_update_tag_action = models.CharField(max_length=10, default="")
+    last_update_tag_id = models.IntegerField(default=0)
+
+    last_update_memory_action = models.CharField(max_length=10, default="")
+    last_update_memory_id = models.IntegerField(default=0)
+
+    # На вход принимается строка с действием и экзепляр тега
+    # del, upd, create,
+    def update_last_edited_tag(self, action, tag):
+        self.last_update_tag_action = action
+        self.last_update_tag_id = tag.id
+        self.save()
+
+    # На вход принимается строка с действием и экзепляр памяти
+    # del, upd, create,
+    def update_last_edited_memory(self, action, memory):
+        self.last_update_memory_action = action
+        self.last_update_memory_id = memory.id
+        self.save()
+
+
 class Tag(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    author = models.ForeignKey(RegularUser, on_delete=models.CASCADE, null=True, blank=True, default=None)
     tag_text = models.CharField(max_length=20)
     count = models.IntegerField(default=0)
 
@@ -26,28 +48,8 @@ class Tag(models.Model):
         return self.count
 
 
-class RegularUser(User):
-    last_update_tag_action = models.CharField(max_length=10, default="")
-    last_update_tag_id = models.IntegerField(default=0)
-
-    last_update_memory_action = models.CharField(max_length=10, default="")
-    last_update_memory_id = models.IntegerField(default=0)
-
-    # На вход принимается строка с действием и экзепляр тега
-    # del, upd, create,
-    def update_last_edited_tag(self, action, tag):
-        self.last_update_tag_action = action
-        self.last_update_tag_id = tag.id
-
-    # На вход принимается строка с действием и экзепляр памяти
-    # del, upd, create,
-    def update_last_edited_tag(self, action, memory):
-        self.last_update_memory_action = action
-        self.last_update_memory_id = memory.id
-
-
 class Memory(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    author = models.ForeignKey(RegularUser, on_delete=models.CASCADE, null=True, blank=True, default=None)
     memory_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', default=timezone.now)
     tags = models.ManyToManyField(Tag)
